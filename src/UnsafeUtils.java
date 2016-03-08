1ae53ba864ef56382ab1fe200714bf833706f58b
slv <slv@ilegendsoft.com>
2016-03-08 18:18:00 +0800

    fix unsafe

--- src/UnsafeUtils.java
+++ src/UnsafeUtils.java
@@ -6,9 +6,21 @@ import java.lang.reflect.Field;
  * Created by shunlv on 16-3-7.
  */
 public class UnsafeUtils {
+  private static Unsafe unsafe;
+
   public static Unsafe getUnsafe() throws Exception {
-    Field f = Unsafe.class.getDeclaredField("theUnsafe");
-    f.setAccessible(true);
-    return (Unsafe) f.get(null);
+    if (unsafe == null) {
+      synchronized (UnsafeUtils.class) {
+        if (unsafe != null) {
+          return unsafe;
+        }
+
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        unsafe = (Unsafe) f.get(null);
+      }
+    }
+
+    return unsafe;
   }
 }
