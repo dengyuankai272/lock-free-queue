6dbc6c519dfe0c57b5f24b095a462433256fb924
slv <slv@ilegendsoft.com>
2016-03-08 18:35:17 +0800

    modify main

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 public class Main {
   public static void main(String[] args) throws Exception {
-//    LockFreeQueue queue = new LinkedListLockFreeQueue();
-    LockFreeQueue queue = new ArrayLockFreeQueue(10);
+    LockFreeQueue queue = new LinkedListLockFreeQueue();
+//    LockFreeQueue queue = new ArrayLockFreeQueue(10);
 
-    Runnable in = new In(queue);
-    Thread in1 = new Thread(in);
-    Thread in2 = new Thread(in);
-    Thread in3 = new Thread(in);
+    Runnable in1 = new In(queue, 0);
+    Runnable in2 = new In(queue, 5);
+    Runnable in3 = new In(queue, 10);
+    Thread inT1 = new Thread(in1);
+    Thread inT2 = new Thread(in2);
+    Thread inT3 = new Thread(in3);
 
     Runnable out = new Out(queue);
     Thread out1 = new Thread(out);
     Thread out2 = new Thread(out);
     Thread out3 = new Thread(out);
 
-    in1.start();
-    in2.start();
-    in3.start();
+    inT1.start();
+    inT2.start();
+    inT3.start();
 
     Thread.sleep(3000);
     System.out.println();
@@ -27,14 +29,16 @@ public class Main {
 
   public static class In implements Runnable {
     private LockFreeQueue queue;
+    private int start;
 
-    public In(LockFreeQueue queue) {
+    public In(LockFreeQueue queue, int start) {
       this.queue = queue;
+      this.start = start;
     }
 
     @Override
     public void run() {
-      for (int i = 0; i < 5; i++) {
+      for (int i = start; i < start + 5; i++) {
         queue.enq(i);
         System.out.println(Thread.currentThread().getName() + "_" + i);
       }
